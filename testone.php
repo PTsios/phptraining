a5257bc86d61a5be7b2858b7860e521446450c4f
PTsios <utreoos92@gmail.com>
2018-07-26 16:41:29 +0300

    lessone 1

---
+++ testone.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paveltsios
+ * Date: 26.07.2018
+ * Time: 16:07
+ */
+
+//$a = -4;
+//$b = 7;
+//
+//
+//if ($a > 0 && $b > 0) {
+//    $result = $a - $b;
+//
+//} elseif ($a < 0 && $b < 0) {
+//    $result = $a * $b;
+//
+//} else {
+//    $result = $a + $b;
+//
+//}
+//echo $result;
+
+
